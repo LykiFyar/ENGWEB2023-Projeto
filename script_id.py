3ef6c94d26e69c21a2068601104650837d0a2156
Marta Sá <martasa57@hotmail.com>
2023-06-19 18:01:21 +0100

    Script para por os ids nos registos

---
+++ script_id.py
@@ -0,0 +1,20 @@
+import json
+import codecs
+import sys 
+
+#py script_id.py nome_do_ficheiro 
+
+input = sys.argv[1]
+id = input.lit("_")[0].upper()
+
+f = open("dataset/"+input, encoding="utf-8")
+registos = json.load(f)
+f.close()
+
+d = 0
+for registo in registos:
+    registo["_id"] = id + str(d)
+    d += 1
+
+with codecs.open("dataset_id/"+input, "w", encoding="utf-8") as arquivo:
+    json.dump(registos, arquivo, ensure_ascii=False)
