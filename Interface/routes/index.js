b434377f032214852a84d68e923febe5fabb7986
Marta Sá <martasa57@hotmail.com>
2023-06-25 13:42:12 +0100

    Teste com a nova BD

--- Interface/routes/index.js
+++ Interface/routes/index.js
@@ -13,5 +13,19 @@ router.get('/', function(req, res, next) {
     })
 });
 
+router.get('/:id', function(req, res, next) {
+  axios.get("http://localhost:5555/acordaos/"+req.params.id)
+      .then(dados=>{
+        for (var d = 0; d < dados.data.length; d++){
+          if (dados.data[d] != null) {
+            acord = dados.data[d]
+          }
+        }
+        res.render('pagAcordao', { acordao: acord });
+      })
+      .catch(erro=>{
+        res.render('error', { error: erro,message:"Erro a obter a página do acordao" });    
+      })  
+});
 
 module.exports = router;
