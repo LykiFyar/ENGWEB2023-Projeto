a70aebd45f1b27e97f4029d1c87bf53ca504b45c
rafa2002 <rafacferreira2002@gmail.com>
2023-06-30 22:23:57 +0100

    pseudo-codigo

--- Interface/public/javascripts/script.js
+++ Interface/public/javascripts/script.js
@@ -13,3 +13,19 @@ function loadPagina(page,pageDirection,querieString){
     window.location.href = "http://localhost:5556/acordaos?"+query+"page="+page+"&pageDirection="+pageDirection;
 }
 
+function favoritos(processos){
+    let favoritos_list = []
+    processos.forEach(processo => {
+        if (processo.fav) favoritos_list.push(processo)
+    });
+    return favoritos_list;
+}
+
+function removerPreferido(processo){
+    processo['fav'] = false;
+}
+
+function adicionarPreferido(processo){
+    processo['fav'] = true;
+}
+
