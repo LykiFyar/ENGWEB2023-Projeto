758d22e51571491da864a013403c3edad2f6cf19
rafa2002 <rafacferreira2002@gmail.com>
2023-06-30 15:51:35 +0100

    Revert "Merge branch 'main' of https://github.com/LykiFyar/EW23_Project"
    This reverts commit b844dd4ce75e95db4a279f9ae0165351b3ed6576, reversing
    changes made to 5b641d91ee6babca72f7e8f4a608a42fe213bf06.

--- API/controler/acordaos.js
+++ API/controler/acordaos.js
@@ -1,47 +1,7 @@
-var Acordaos = require('../models/acordaos')
-
-var project = {"Processo":1, "Data do Acordão":1, "Relator":1, "Descritores":1}
-/*
-module.exports.list = (limit, next_id) =>{
-    return Acordaos.aggregate
-    ([
-        {
-            $match: {'_id': {'$gte': next_id} }
-        },
-        {   
-            $sort: {_id:1}
-        },
-        {
-            $project: project
-        },
-        {
-            $facet:
-            {
-                "Registos":
-                [
-                    {
-                        $limit: limit
-                    }
-                ],
-                "Paginas":
-                [
-                    {
-                        "$count":"Total"
-                    }
-                ]
-            } 
-        }
-    ])
-    .then(dados=>{
-        return dados
-    })
-    .catch(erro=>{
-        return erro
-    })
-}*/
+var Arcordaos = require('../models/acordaos')
 
 module.exports.list = (limit, next_id) =>{
-    return Acordaos.find({'_id': {'$gte': next_id}}, project).sort("_id").limit(limit)
+    return Arcordaos.find({'_id': {'$gte': next_id}}, {"Processo":1, "Data do Acordão":1, "tribunal":1,"Relator":1, "Descritores":1}).sort("_id").limit(limit)
                 .then(dados=>{
                     return dados
                 }
@@ -52,7 +12,7 @@ module.exports.list = (limit, next_id) =>{
 }
 
 module.exports.getAcordao = id =>{
-    return Acordaos.findOne({_id:id})
+    return Arcordaos.findOne({_id:id})
                 .then(dados=>{
                     return dados
                 }
@@ -65,7 +25,7 @@ module.exports.getAcordao = id =>{
 
 module.exports.acordaosDataDesde = (data, limit, next_id) =>{
     var date = new Date(data)
-    return Acordaos.aggregate([{$match: { 
+    return Arcordaos.aggregate([{$match: { 
                                         $expr: {
                                             $gte:[
                                                 {$dateFromString: {dateString:'$Data do Acordão'}}, 
@@ -73,7 +33,7 @@ module.exports.acordaosDataDesde = (data, limit, next_id) =>{
                                             ]
                                         }
                                     }
-                         }],project).sort("_id").limit(limit)
+                         }],{"Processo":1, "Data do Acordão":1, "tribunal":1, "Relator":1, "Descritores":1}).sort("_id").limit(limit)
                 .then(dados=>{
                     return dados
                 }
@@ -87,11 +47,9 @@ module.exports.acordaosFilter = (queries, limit, next_id, pageDirection) => {
     delete queries.page
     delete queries.pageDirection
 
-    console.log(next_id, pageDirection)
-
     if (!pageDirection) {
         queries["_id"] = {'$lt': next_id}
-        return Acordaos.find(queries, project).sort({_id:-1}).limit(limit)
+        return Arcordaos.find(queries, {"Processo":1}).sort({_id:-1}).limit(limit)
                     .then(dados=>{
                         return dados
                     })
@@ -101,7 +59,7 @@ module.exports.acordaosFilter = (queries, limit, next_id, pageDirection) => {
     }
     else{
         queries["_id"] = {'$gt': next_id}
-        return Acordaos.find(queries, project).sort({_id:1}).limit(limit)
+        return Arcordaos.find(queries, {"Processo":1}).sort({_id:1}).limit(limit)
                     .then(dados=>{
                         return dados
                     }
@@ -114,7 +72,7 @@ module.exports.acordaosFilter = (queries, limit, next_id, pageDirection) => {
 
 
 module.exports.addAcordao = (acordao) => {
-    return Acordaos.collection.insertOne(acordao)
+    return Arcordaos.collection.insertOne(acordao)
                 .then(dados=>{
                     return dados
                 }
@@ -126,7 +84,7 @@ module.exports.addAcordao = (acordao) => {
 
 
 module.exports.editAcordao = (id,acordao)=>{
-    return Acordaos.updateOne({_id:id},acordao)
+    return Arcordaos.updateOne({_id:id},acordao)
             .then(dados=>{
                 return dados
             }
@@ -138,7 +96,7 @@ module.exports.editAcordao = (id,acordao)=>{
 
 
 module.exports.deleteAcordao = id =>{
-    return Acordaos.deleteOne({_id:id})
+    return Arcordaos.deleteOne({_id:id})
                 .then(dados=>{
                     return dados
                 }
--- Interface/routes/index.js
+++ Interface/routes/index.js
@@ -1,6 +1,5 @@
 var express = require('express');
 var router = express.Router();
-var env = require('../config/env')
 var axios = require("axios")
 
 
@@ -15,12 +14,6 @@ var campos = ['Nº do Documento',' Nº Convencional','Data da Decisão','Data','
               'Texto das Cláusulas Abusivas','Recursos','Referência de Publicação','Processo no Tribunal Recurso','Tema',
               'Processo no Tribunal Recorrido','Parecer Ministério Publico','Peça Processual','Texto Parcial','Reclamações','Tribunal Recorrido','Referência a Doutrina','Doutrina']
 
-var tribunais= {"Tribunal Constitucional": "atco1", "Tribunal dos Conflitos": "jcon", "Cláusulas Abusivas Julgadas pelos Tribunais": "jdgpj", 
-                "Supremo Tribunal Administrativo": "jsta", "Supremo Tribunal de Justiça": "jstj", "Tribunal Central Administrativo Sul": "jtca", 
-                "Tribunal Central Administrativo Sul - Contencioso Administrativo": "jtcampca", "Tribunal Central Administrativo Sul - Contencioso Tributário": "jtcampct", 
-                "Tribunal Central Administrativo Norte": "jtcn", "Tribunal da Relação de Coimbra": "jtrc", "Tribunal da Relação de Évora": "jtre", 
-                "Tribunal da Relação de Guimarães": "jtrg", "Tribunal da Relação de Lisboa": "jtrl", "Tribunal da Relação de Porto": "jtrp" } 
-
 
 /* GET home page. */
 router.get('/acordaos', function(req, res, next) {
@@ -32,11 +25,11 @@ router.get('/acordaos', function(req, res, next) {
     if (req.query[key] != '' && key != "page" && key != "pageDirection"){
         query += key + "=" + req.query[key] + "&"
     }
-  }
+}
 
   axios.get("http://localhost:5555/acordaos?"+query+"page="+page+"&pageDirection="+pageDirection)
     .then(dados=>{
-      res.render('main', { processos: dados.data, queries: req.query, page: page, pageDirection: pageDirection, tribunais: tribunais});
+      res.render('main', { processos: dados.data, queries: req.query, page: page, pageDirection: pageDirection});
     })
     .catch(erro=>{
       res.render('error', { error: erro,message:"Erro a obter lista de acordaos" });
@@ -45,14 +38,9 @@ router.get('/acordaos', function(req, res, next) {
 
 
 router.get('/acordaos/:id', function(req, res, next) {
-  for (let key in req.query){
-    if (key != "prevUrl" && key != "page" && key != "pageDirection")
-      prevUrl += key + "=" + req.query[key] + "&"
-  }
-
   axios.get("http://localhost:5555/acordaos/"+req.params.id)
       .then(acordao=>{
-        res.render('pagAcordao', { acordao: acordao.data, tribunais: tribunais});
+        res.render('pagAcordao', { acordao: acordao.data });
       })
       .catch(erro=>{
         res.render('error', { error: erro,message:"Erro a obter a página do acordao" });    
@@ -71,18 +59,12 @@ router.get('/edit/:id', function(req,res){
 })
 
 
-router.get('/delete/:id', function(req, res) {
-  axios.delete("http://localhost:5555/acordaos/"+req.params.id)
-  res.redirect('/acordaos');
-});
-
 router.get('/add', function(req, res) {
   res.render('addForm', {campos: campos});
 });
 
-
-router.get('/alteracoes', function(req, res){
-  res.render('alteracoes')
+router.get('/faq', function(req, res){
+  res.render('faq')
 })
 
 
@@ -90,16 +72,9 @@ router.get('/login', function(req, res){
   res.render('loginForm')
 })
 
-router.get('/register', function(req, res){           
-  res.render('registerForm')
-})
-
 
 router.post('/edit/:id', function(req, res) {
-  const descritores = req.body.Descritores.split('\n').map(linha => linha.toUpperCase())
-  delete req.body.Descritores
-  req.body["Descritores"] = descritores
-
+  console.log(req.body)
   axios.put("http://localhost:5555/acordaos/"+req.params.id, req.body)
     .then(acordao => {
       //res.render('editConfirm', {a: acordao})
@@ -115,11 +90,6 @@ router.post('/add', function(req, res) {
   const _id = parseInt(req.body._id)
   delete req.body._id
   req.body["_id"] = _id
-
-  const descritores = req.body.Descritores.split('\n').map(linha => linha.toUpperCase())
-  delete req.body.Descritores
-  req.body["Descritores"] = descritores
-  
   axios.post("http://localhost:5555/acordaos", req.body)
     .then(acordao => {
       //res.render('editConfirm', {a: acordao})
@@ -131,29 +101,19 @@ router.post('/add', function(req, res) {
 })
 
 
-
+/*
 router.post('/login', function(req, res){
-  axios.post(env.authAccessPoint +'/login', req.body)
+  axios.post('http://localhost:5557/users/login', req.body)
     .then(response => {
       res.cookie('token', response.data.token)
-      res.redirect('/acordaos')
+      res.redirect('/')
     })
     .catch(e =>{
       res.render('error', {error: e, message: "Credenciais inválidas"})
     })
 })
 
-router.post("/register", (req, res) => {
-  axios.post(env.authAccessPoint + "/register", req.body)
-    .then(response => {
-      res.redirect('/')
-    })
-    .catch(err => {
-      res.render('error', {error: err})
-    })
-})
 
-/*
 router.get('/logout', verificaToken, (req, res) => {
   res.cookie('token', "revogado.revogado.revogado")
   res.redirect('/')
