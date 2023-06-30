7c4f63634222359932d50675e99d461a731bf331
rafa2002 <rafacferreira2002@gmail.com>
2023-06-30 15:51:31 +0100

    Revert "new things"
    This reverts commit cc554ffd6c19e11fd6613cded84e8039fb24dc5f.

--- API/controler/acordaos.js
+++ API/controler/acordaos.js
@@ -1,7 +1,47 @@
-var Arcordaos = require('../models/acordaos')
+var Acordaos = require('../models/acordaos')
+
+var project = {"Processo":1, "Data do Acordão":1, "Relator":1, "Descritores":1}
+/*
+module.exports.list = (limit, next_id) =>{
+    return Acordaos.aggregate
+    ([
+        {
+            $match: {'_id': {'$gte': next_id} }
+        },
+        {   
+            $sort: {_id:1}
+        },
+        {
+            $project: project
+        },
+        {
+            $facet:
+            {
+                "Registos":
+                [
+                    {
+                        $limit: limit
+                    }
+                ],
+                "Paginas":
+                [
+                    {
+                        "$count":"Total"
+                    }
+                ]
+            } 
+        }
+    ])
+    .then(dados=>{
+        return dados
+    })
+    .catch(erro=>{
+        return erro
+    })
+}*/
 
 module.exports.list = (limit, next_id) =>{
-    return Arcordaos.find({'_id': {'$gte': next_id}}, {"Processo":1, "Data do Acordão":1, "tribunal":1,"Relator":1, "Descritores":1}).sort("_id").limit(limit)
+    return Acordaos.find({'_id': {'$gte': next_id}}, project).sort("_id").limit(limit)
                 .then(dados=>{
                     return dados
                 }
@@ -12,7 +52,7 @@ module.exports.list = (limit, next_id) =>{
 }
 
 module.exports.getAcordao = id =>{
-    return Arcordaos.findOne({_id:id})
+    return Acordaos.findOne({_id:id})
                 .then(dados=>{
                     return dados
                 }
@@ -25,7 +65,7 @@ module.exports.getAcordao = id =>{
 
 module.exports.acordaosDataDesde = (data, limit, next_id) =>{
     var date = new Date(data)
-    return Arcordaos.aggregate([{$match: { 
+    return Acordaos.aggregate([{$match: { 
                                         $expr: {
                                             $gte:[
                                                 {$dateFromString: {dateString:'$Data do Acordão'}}, 
@@ -33,7 +73,7 @@ module.exports.acordaosDataDesde = (data, limit, next_id) =>{
                                             ]
                                         }
                                     }
-                         }],{"Processo":1, "Data do Acordão":1, "tribunal":1, "Relator":1, "Descritores":1}).sort("_id").limit(limit)
+                         }],project).sort("_id").limit(limit)
                 .then(dados=>{
                     return dados
                 }
@@ -47,9 +87,11 @@ module.exports.acordaosFilter = (queries, limit, next_id, pageDirection) => {
     delete queries.page
     delete queries.pageDirection
 
+    console.log(next_id, pageDirection)
+
     if (!pageDirection) {
         queries["_id"] = {'$lt': next_id}
-        return Arcordaos.find(queries, {"Processo":1}).sort({_id:-1}).limit(limit)
+        return Acordaos.find(queries, project).sort({_id:-1}).limit(limit)
                     .then(dados=>{
                         return dados
                     })
@@ -59,7 +101,7 @@ module.exports.acordaosFilter = (queries, limit, next_id, pageDirection) => {
     }
     else{
         queries["_id"] = {'$gt': next_id}
-        return Arcordaos.find(queries, {"Processo":1}).sort({_id:1}).limit(limit)
+        return Acordaos.find(queries, project).sort({_id:1}).limit(limit)
                     .then(dados=>{
                         return dados
                     }
@@ -72,7 +114,7 @@ module.exports.acordaosFilter = (queries, limit, next_id, pageDirection) => {
 
 
 module.exports.addAcordao = (acordao) => {
-    return Arcordaos.collection.insertOne(acordao)
+    return Acordaos.collection.insertOne(acordao)
                 .then(dados=>{
                     return dados
                 }
@@ -84,7 +126,7 @@ module.exports.addAcordao = (acordao) => {
 
 
 module.exports.editAcordao = (id,acordao)=>{
-    return Arcordaos.updateOne({_id:id},acordao)
+    return Acordaos.updateOne({_id:id},acordao)
             .then(dados=>{
                 return dados
             }
@@ -96,7 +138,7 @@ module.exports.editAcordao = (id,acordao)=>{
 
 
 module.exports.deleteAcordao = id =>{
-    return Arcordaos.deleteOne({_id:id})
+    return Acordaos.deleteOne({_id:id})
                 .then(dados=>{
                     return dados
                 }
--- Auth/routes/user.js
+++ Auth/routes/user.js
@@ -25,9 +25,9 @@ router.post('/', auth.verificaAcesso, function(req, res){
     .catch(e => res.status(500).jsonp({error: e}))
 })
 
-router.post('/register', auth.verificaAcesso, function(req, res) {
+router.post('/register', function(req, res) {
   var d = new Date().toISOString().substring(0,19)
-  userModel.register(new userModel({ username: req.body.username, filiation: req.body.filiation, name: req.body.name, 
+  userModel.register(new userModel({ username: req.body.username, filiation: req.body.filiation, name: req.body.name, email: req.body.email, 
                                       level: req.body.level, active: true, dateCreated: d, favorites: [] }), 
                 req.body.password, 
                 function(err, user) {
